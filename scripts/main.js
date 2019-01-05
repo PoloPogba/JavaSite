b4ecbc2581cc8dabda850d316ed23db1bccf19e2
PoloPogba <polo_pogba@yahoo.com>
2019-01-05 22:16:49 +0100

    Ajout du lien GROS SON MAMENE

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,14 @@
-setTimeout(function(){ alert("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please"); }, 10000);
+ var start, result;
+                start = 'Hey le site xxxxvidsxxxx est trop bien. Viens dessus stp please';                      
+                 
+ 
+                        function YEAH(){
+        
+                        if (confirm(start)){
+                        location.href = ("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+                        } 
+                        }
+        
+
+                setTimeout(YEAH, 3000);
 
