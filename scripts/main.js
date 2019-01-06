c81e4f9f81ad6fd29940221a4c4c2a9bdabbd682
PoloPogba <polo_pogba@yahoo.com>
2019-01-06 03:01:59 +0100

    Bouton PSY

--- scripts/main.js
+++ scripts/main.js
@@ -12,3 +12,30 @@
 
                 setTimeout(YEAH, 3000);
 
+
+var btn = document.querySelector('input');
+var lol = 0;
+btn.addEventListener('click', updateBtn);
+
+function updateBtn() {
+  if (btn.value === "VOUS N'ETES PAS PRET !") {
+    btn.value = 'ASSEZ ?';
+    lol = 1; 
+    document.body.bgColor='magenta';
+    document.body.style.color='blue';
+    
+		  
+    document.paragraph.style.fontSize = "45px";
+    document.body.style.color= 'blue';
+
+	  document.p.style.fontFamily = "Courier New", Courier, monospace;
+	  
+
+  } else {
+    btn.value = "VOUS N'ETES PAS PRET !";
+    lol = 0;
+  document.body.bgColor='white';
+	  document.body.style.color='black';
+  }
+}
+
